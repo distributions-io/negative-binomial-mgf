76d13e72fe8a9e42e36182579acd344d72df83f4
Planeshifter <pburckhardt@outlook.com>
2015-08-08 16:22:13 +0200

    [INIT]

---
+++ test/fixtures/test.deepset.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+r = 1
+p = 0.5
+d = NegativeBinomial( r,p )
+
+x = linspace( .5, -100, 100 )
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("r", r),
+	("p", p),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/deepset.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
---
+++ test/fixtures/test.number.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+r = 1
+p = 0.5
+d = NegativeBinomial( r,p )
+
+x = [ -5, -2.5, 0, 2.5, 5 ]
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("r", r),
+	("p", p),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/number.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
